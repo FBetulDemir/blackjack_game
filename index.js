e0ab37fffbca16e97a40dd3ad92c2db2be922c64
FBetulDemir <fatmabetuld@gmail.com>
2024-03-27 08:03:38 +0100

    Created the functions for the buttons in index.js

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 let firstCard = 10;
 let secondCard = 7;
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isAlive = true;
 let message = ""
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
-    
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳";
-    hasBlackJack = true;
-} else {
-    message = "You're out of the game! 😭";
-    isAlive = false;
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.querySelector("#cards-el");
+
+function getRandomCard(){
+    return
 }
 
-console.log(message);
+//fuction starts the game
+function startGame(){
+    renderGame()
+}
+
+function renderGame(){
+    cardsEl.textContent = "Cards: "
+    for(let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " - ";
+    }
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂";
+        
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game! 😭";
+        isAlive = false;
+    }
+    
+    messageEl.textContent = message;
+    sumEl.textContent = "Sum: " + sum;
+    //cardsEl.textContent = "Cards: " + cards[0] + " - " + cards[1];
+}
+
+function newCard(){
+    let thirdCard = 5;
+    cards.push(thirdCard);
+    sum = firstCard + secondCard + thirdCard;
+    renderGame()
+}
